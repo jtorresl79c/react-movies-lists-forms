1cf3651e6eb8afcf872d2b04891777a87f041ece
Jason Torres Luis <jtorresl79c@gmail.com>
2022-02-28 21:49:26 -0800

    10. Building a Bootstrap Form - Aqui se puso el truco del wrap y el uso del htmlFor en vez del for en las etiquetas label html

---
+++ src/components/loginForm.jsx
@@ -0,0 +1,26 @@
+import React, { Component } from 'react'
+
+export default class loginForm extends Component {
+    render() {
+        return (
+            // Select text to wrap -> CTRL+SHIFT+P -> Write wrap
+            <div>
+                <h1>Login</h1>
+                <form>
+                    <div className="mb-3">
+                        {/* Recordemos que JSX es mas cercano a Javascript que ha HTML, por lo tanto no podemos usar 'for' porque
+                        eso es una palabra reservada para el 'for loop' por eso cuando queramos usar el atributo 'for' de los
+                        label de html, en vez de poner 'for' se usa el 'htmlFor' */}
+                        <label htmlFor="username" className="form-label">Username</label>
+                        <input type="email" className="form-control" id="username" />
+                    </div>
+                    <div className="mb-3">
+                        <label htmlFor="password" className="form-label">Password</label>
+                        <input type="password" className="form-control" id="password" />
+                    </div>
+                    <button type="submit" className="btn btn-primary">Login</button>
+                </form>
+            </div>
+        )
+    }
+}
