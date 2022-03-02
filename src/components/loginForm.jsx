062eef5d1961e7bab3a9067308fbd8be0f294544
Jason Torres Luis <jtorresl79c@gmail.com>
2022-03-01 22:29:05 -0800

    4. Refs - El tema de refs es el tema de Uncontrolled Components, cuando se y es cuando no queremos usar el Single Source of Truth

--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -1,11 +1,21 @@
 import React, { Component } from 'react'
 
 export default class loginForm extends Component {
+    username = React.createRef(); // Refs
+    
+    componentDidMount(){
+        this.username.current.focus() // Un ejemplo en donde lo podriamos usar, cuando el componente cargue, automaticamente
+        // se pondria en focus este input, aunque otra solucion mejor si quisieramos agregar este comportamiento seria poner
+        // el atributo autoFocus directamente en el Input. Por eso nos dicen que tengamos cuidado con los refs, ya que utilizando
+        // el single source of truth es la forma normal para manipular datos en vez de los refs.
+    }
+
     handleSubmit = e => {
         e.preventDefault()
 
         // Call the server
-        console.log('Submitted')
+        const username = this.username.current.value // Refs
+        console.log(username)
     }
 
     render() {
@@ -21,7 +31,7 @@ export default class loginForm extends Component {
                         eso es una palabra reservada para el 'for loop' por eso cuando queramos usar el atributo 'for' de los
                         label de html, en vez de poner 'for' se usa el 'htmlFor' */}
                         <label htmlFor="username" className="form-label">Username</label>
-                        <input type="email" className="form-control" id="username" />
+                        <input type="email" ref={this.username} className="form-control" id="username" /> {/* Refs */}
                     </div>
                     <div className="mb-3">
                         <label htmlFor="password" className="form-label">Password</label>
