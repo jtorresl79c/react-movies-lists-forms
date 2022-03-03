13563fec8f7f62c849df03af18685e3c9bad10b0
Jason Torres Luis <jtorresl79c@gmail.com>
2022-03-02 22:26:16 -0800

    8. Extracting a Reusable Input - En loginForm.jsx hay varios input, el codigo de los inputs es muy repetitivo, y por ello facilmente lo pasamos a un component

--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -1,24 +1,25 @@
 import React, { Component } from 'react'
+import Input from './common/input'
 
 export default class loginForm extends Component {
-    constructor(props){
+    constructor(props) {
         super(props)
         this.state = {
             account: { username: '', password: '' }
         }
     }
-    
-    componentDidMount(){
+
+    componentDidMount() {
 
     }
 
 
-    handleChange = ( { currentTarget: input } ) => { // ': input' es como si fuera un 'as', por lo que es un alias
+    handleChange = ({ currentTarget: input }) => { // ': input' es como si fuera un 'as', por lo que es un alias
         const account = { ...this.state.account }
 
         account[input.name] = input.value
         console.log(account)
-        this.setState({account})
+        this.setState({ account })
     }
 
     handleSubmit = e => {
@@ -37,17 +38,20 @@ export default class loginForm extends Component {
             <div>
                 <h1>Login</h1>
                 <form onSubmit={this.handleSubmit}>
-                    <div className="mb-3">
-                        {/* Recordemos que JSX es mas cercano a Javascript que ha HTML, por lo tanto no podemos usar 'for' porque
-                        eso es una palabra reservada para el 'for loop' por eso cuando queramos usar el atributo 'for' de los
-                        label de html, en vez de poner 'for' se usa el 'htmlFor' */}
+                    {/* <div className="mb-3">
                         <label htmlFor="username" className="form-label">Username</label>
                         <input type="text" name="username" className="form-control" id="username" value={account.username} onChange={ this.handleChange  } />
                     </div>
                     <div className="mb-3">
                         <label htmlFor="password" className="form-label">Password</label>
                         <input type="password" name="password" value={account.password} onChange={ this.handleChange  } className="form-control" id="password" />
-                    </div>
+                    </div> */}
+
+                    {/* El codigo de los input, por lo que podemos pasarlo a un componente */}
+                    <Input name="username" label="Username" onChange={this.handleChange} value={this.state.account.username} />
+                    <Input name="password" label="Password" onChange={this.handleChange} value={this.state.account.password} />
+
+
                     <button type="submit" className="btn btn-primary">Login</button>
                 </form>
             </div>
