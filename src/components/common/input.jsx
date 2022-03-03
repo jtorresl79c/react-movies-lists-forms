b94b5e799486fda9ab9c3c9fe4f62c511ea268a2
Jason Torres Luis <jtorresl79c@gmail.com>
2022-03-02 22:27:49 -0800

    8. Extracting a Reusable Input - en input.jsx se aplica destructuracion

--- src/components/common/input.jsx
+++ src/components/common/input.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 
-export default function input(props) {
+export default function input({ name, label, value, onChange }) {
     return (
         <div className="mb-3">
             {/* Recordemos que JSX es mas cercano a Javascript que ha HTML, por lo tanto no podemos usar 'for' porque
             eso es una palabra reservada para el 'for loop' por eso cuando queramos usar el atributo 'for' de los
             label de html, en vez de poner 'for' se usa el 'htmlFor' */}
-            <label htmlFor={props.name} className="form-label">{props.label}</label>
-            <input type="text" name={props.name} className="form-control" id={props.name} value={props.value} onChange={props.onChange} />
+            <label htmlFor={name} className="form-label">{label}</label>
+            <input type="text" name={name} className="form-control" id={name} value={value} onChange={onChange} />
         </div>
     )
 }
