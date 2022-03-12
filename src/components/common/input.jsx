d4c885bc498b889a2acd852f0ea059d6118947f1
Jason Torres Luis <jtorresl79c@gmail.com>
2022-03-12 00:16:40 -0800

    Anteriormente agregamos el argunmento type para que asi los types de los input fueran dinamicos, el problema es que se tuvo que ir input.jsx para mandar a llmar el type y poder setearlo, logicamente es lo que se hace porque se esta mandando otro argumento, el problema es que siempre que queramos mandar un nuevo argumento al component input.jsx se tienen que ir y declarar, para evitar eso podemos usar destructuracion, en input.jsx se puso: name, label, error, ...rest, esto para que en el input podamos usar el spread operator para poder declarar todas las propiedades, esto funciona porque el nombre de la propiedad es el mismo nombre del valor de la variable que lo llena

--- src/components/common/input.jsx
+++ src/components/common/input.jsx
@@ -1,6 +1,10 @@
 import React from 'react'
 
-export default function input({ name, label, value, onChange, error, type }) { // No importa el orden en el que
+// name, label y error se mantuvieron porque la etiqueta label lo usa de forma separado, asi como el id en la etiqueta
+// input y el error lo utiliza la etiqueta div
+
+// se pone ...rest como forma de declaracion
+export default function input({ name, label, error, ...rest}) { // No importa el orden en el que
     // los establescamos, porque los estamos destructurando, SE RECIBE UN OBJETO no parametros de funciones.
     return (
         <div className="mb-3">
@@ -8,7 +12,10 @@ export default function input({ name, label, value, onChange, error, type }) { /
             eso es una palabra reservada para el 'for loop' por eso cuando queramos usar el atributo 'for' de los
             label de html, en vez de poner 'for' se usa el 'htmlFor' */}
             <label htmlFor={name} className="form-label">{label}</label>
-            <input type={type} name={name} className="form-control" id={name} value={value} onChange={onChange} />
+            {/* se pone {...rest} para que se establescan todas las propiedades */}
+            {/* name={name} se pone porque ...rest TRAE TODAS LAS PROPIEDADES excepto las ya declaradas (que en esta caso
+                son name, label y error)*/} 
+            <input name={name} className="form-control" id={name} {...rest}/>
             { error && <div className="alert alert-danger">{error}</div>}
         </div>
     )
