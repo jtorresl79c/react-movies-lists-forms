6362708e56cdb74f55e9b63e2a03f0667527b7e5
Jason Torres Luis <jtorresl79c@gmail.com>
2022-03-27 22:45:31 -0700

    23. Code Review - select.jsx fue una decision correcta el crearlo

--- src/components/common/select.jsx
+++ src/components/common/select.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
 
 export default function select({ data, onChange, name, value, selectedPropertyName, label }) {
+    // Como ultima cosa, defiende el porque hicimos un select.jsx aparte y no usamos otra vez el input.jsx
+    // si hubieramos usado el input.jsx se hubiera tenido que agregar muchos if en orden para que el select
+    // funcionara como queriamos, por lo que hacer un componente aparte select.jsx fue la desicion correcta
     return (
         <div className="mb-3">
             <label htmlFor={name} className="form-label">{label}</label>
