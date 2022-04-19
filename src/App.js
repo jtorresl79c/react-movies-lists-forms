e65645ecc1c9ee557ebafe66988acdef6a1e3baf
jtorresl79c <83847060+jtorresl79c@users.noreply.github.com>
2022-04-19 11:28:24 -0700

    9. Authentication and Authorization - 11- Getting the Current User - En App.js decodificamos el jwt para obtener los datos del usuario logueado, como lo obtenemos en el componente padre podremos pasar esta info a cualquiera de los componente por medio de props

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import React, { Component } from "react";
 import { Route, Redirect, Switch } from "react-router-dom";
+
+import jwtDecode from "jwt-decode";
+
 import Movies from "./components/movies";
 import MovieForm from "./components/movieForm";
 import Customers from "./components/customers";
@@ -12,6 +15,28 @@ import NewMovie from "./components/newMovie";
 import "./App.css";
 
 class App extends Component {
+
+    constructor(props){
+        super(props)
+        this.state = {
+            user: {}
+        }
+    }
+
+    componentDidMount(){
+        try {
+            const jwt = localStorage.getItem('token')
+            const user = jwtDecode(jwt) // Si la variable token esta vacia (que no exista en el localStorage el token)
+            // entonces al hacer esto jwtDecode(null) habra un error, podriamos pensar que antes de ejecutar esta linea
+            // de codigo comprobar si el token existe o no, en el caso de no existir reedireccionar, el problema es que
+            // si nos fijamos bien este archivo es el App.js osea que SIEMPRE se va a ejecutar y si bien podemos reedireccionar
+            // eso no evitara que se arroge un error, podemos usar un try catch para evitar el error y despues reedireccionar al
+            // login
+            this.setState({user})
+        } catch (ex) {
+        }
+    }
+
     render() {
         return (
             <React.Fragment>
